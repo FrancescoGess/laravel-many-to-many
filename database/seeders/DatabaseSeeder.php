4bff2043ceec8f416328217bd370f4e0181392b1
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-04-04 17:56:02 +0200

    seed e rename project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,9 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            PostSeeder::class,
-            TypeSeeder::class
+            ProjectSeeder::class,
+            TypeSeeder::class,
+            TechnologySeeder::class
         ]);
     }
 }
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +15,20 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            'coding',
+            'laravel',
+            'vue',
+            'js',
+            'python'
+        ];
+
+        foreach($technologies as $element){
+            $new_technology = new Technology();
+            $new_technology->name = $element;
+            $new_technology->slug = Str::slug($new_technology->name);
+            $new_technology->save();
+
+        }
     }
 }
